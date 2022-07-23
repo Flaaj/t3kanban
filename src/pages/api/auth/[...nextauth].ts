c19daa5445147bbaf7ddf064419da17948523af9
Michał Wołek <32792800+Flaaj@users.noreply.github.com>
2022-07-23 22:15:08 +0200

    log session context

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -13,7 +13,7 @@ export const authOptions: NextAuthOptions = {
   //   signIn: "/auth/signin",
   //   error: "/auth/error",
   // },
-  secret: process.env.NEXTAUTH_SECRET,
+  secret: "cl5ybvrv5000009mt9n0635oc",
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID!,
