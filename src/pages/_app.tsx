114ffffea089927b96c6630376b99198293bd8fc
Michał Wołek <32792800+Flaaj@users.noreply.github.com>
2022-07-23 21:49:32 +0200

    .

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -36,7 +36,6 @@ export default withTRPC<AppRouter>({
     /**
      * If you want to use SSR, you need to use the server's full URL
      * @link https://trpc.io/docs/ssr
-     * 
      */
     const url = `${getBaseUrl()}/api/trpc`;
 
